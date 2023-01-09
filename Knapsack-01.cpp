8b869985117b8eae7fb9a3a1440384b75ab194b8
RohitBeniwal <beniwalrb177@gmail.com>
2023-01-09 16:51:53 +0530

    k-1

--- Knapsack-01.cpp
+++ Knapsack-01.cpp
@@ -13,15 +13,27 @@ ios::sync_with_stdio(0);
 cin.tie(0);
     ll n=4;
     ll m=8;
-    vector<ll> p{1,2,5,6};
-    vector<ll> w{2,3,4,5};
+    ll p[4]={1,2,5,6};
+    ll w[4]={2,3,4,5};
     ll v[n+1][m+1]={0};
+    rep(i,0,m+1){
+        v[0][i]=0;
+    }
+    
     rep(i,1,n+1){
         rep(j,1,m+1){
-            v[i][j]=max(v[i-1][j],v[i-1][j-w[i]]+p[i]);
+            if((j-w[i-1])>=0)
+            v[i][j]=max(v[i-1][j],v[i-1][j-w[i-1]]+p[i-1]);
+            else
+            v[i][j]=v[i-1][j];
         }
     }
-    // cout<<1<<endl;
+    // rep(i,0,n+1){
+    //     rep(j,0,m+1){
+    //         cout<<v[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
     cout<<v[n][m]<<endl;
     
 
